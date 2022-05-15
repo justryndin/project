741644de633eda940f6c85b873b22c002c542034
Ivan <ivanryndin89@gmail.com>
2022-05-15 15:14:59 +0300

    Рассчитать на какую сумму лежит каждого товара на складе.

--- practice.py
+++ practice.py
@@ -1,24 +1,4 @@
-class MyClass:
-    name = "Класс MyClass"
+def persistence(n):
+   pass
 
-    def set(self, n):
-        self.nickname = n
-
-    def get(self):
-        print(f"Значение поля: {self.nickname}")
-
-    def __init__(self, n):
-        self.set(n)
-        print("Экзепляр класса создан!")
-        self.get()
-
-
-green = MyClass("Зеленый")
-print("Принадлежность", green.name)
-red = MyClass("Красный")
-print("Принадлежность", red.name)
-MyClass.name = "Здесь могла быть ваша реклама"
-print("Спрашивает Красный", red.name)
-print("Спрашивает Зеленый", green.name)
-
-print(5 != 6)
+persistence(39)
