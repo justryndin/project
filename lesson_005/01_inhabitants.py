150e4a16ab62e831879bf0eaa084778383fedca4
Ivan <ivanryndin89@gmail.com>
2022-05-20 18:52:24 +0300

    Вывести на консоль жителей комнат (модули room_1 и room_2)

---
+++ lesson_005/01_inhabitants.py
@@ -0,0 +1,10 @@
+from room_1 import folks as rm_1
+from room_2 import folks as rm_2
+
+# Вывести на консоль жителей комнат (модули room_1 и room_2)
+# Формат: В комнате room_1 живут: ...
+
+# TODO здесь ваш код
+
+print(f'В комнате room_1 живут: {", ".join(rm_1)}.')
+print(f'В комнате room_2 живут: {", ".join(rm_2)}.')
